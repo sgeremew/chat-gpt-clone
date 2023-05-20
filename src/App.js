7d4c8134629dcf022d70b8d69ceb6c3fe915f726
geremews <samuel.geremew@specinnovations.com>
2023-05-20 10:14:15 -0400

    simple layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <section className={"sidebar"}>
-        <button>New Chat</button>
+        <button>+ New Chat</button>
         <ul className={"history"}>
-
+            <li>stuff here</li>
         </ul>
         <nav>
           <p>Made by Sam</p>
         </nav>
       </section>
-      <section className={"main"}></section>
+      <section className={"main"}>
+          <h1>SamGPT</h1>
+          <ul className={"feed"}>
+
+          </ul>
+          <div className={"bottom-section"}>
+              <div className={"input-container"}>
+                  <input/>
+                  <div id={"submit"}>➢</div>
+              </div>
+              <p className={"info"}>
+                  ChatGPT clone preview. This is a beta using the ChatGPT API.
+              </p>
+          </div>
+      </section>
 
     </div>
   );
