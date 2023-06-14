cc7ee6428e73fb80d996f3b4249282724429fc46
geremews <samuel.geremew@specinnovations.com>
2023-06-13 22:21:15 -0400

    require dotenv to access environment variable

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const PORT = 8000;
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
